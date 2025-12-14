37672797b901776b6b58da441788bbe04098f407
Akashkavindu <143059042+Akashkavindu@users.noreply.github.com>
2025-12-14 06:04:11 -0800

    ⚡🔥ZANTA🔥⚡

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ const FINAL_ALIVE_MSG = ALIVE_MSG_TEMPLATE
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "sE8AEAqB#N8IswjTJ8U4VdoFH3vw8KtaJZYX4NEkhjz_tuT6faA0", //Your session id
+SESSION_ID: process.env.SESSION_ID || "Ic8hhZDQ#DXDftm_fCmoaVSFw8UjrYshaiA6gqMRM4s31dqyjrI0", //Your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Akashkavindu/ZANTA_MD/blob/main/images/alive-new.jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || FINAL_ALIVE_MSG, 
 BOT_OWNER: OWNER_NUMBER, 
